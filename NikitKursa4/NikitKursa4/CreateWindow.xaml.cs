7e9bef18c763262e74a55822dc53bf79a94becc8
NikitaZhuravlevvv <77377535+NikitaZhuravlevvv@users.noreply.github.com>
2021-06-05 14:24:19 +0300

    Add files via upload

---
+++ NikitKursa4/NikitKursa4/CreateWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NikitKursa4
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateWindow.xaml
+    /// </summary>
+    public partial class CreateWindow : Window, INotifyPropertyChanged
+    {
+
+        public List<CompanyNikit> WorkList { get; set; }
+
+        public ContractsNikit CurrentService { get; set; }
+
+        public string WindowName
+        {
+            get
+            {
+                return CurrentService.Id == 0 ? "Новый сотрудник" : "Редактирование сотрудника";
+            }
+        }
+
+        public CreateWindow(ContractsNikit Contract)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            CurrentService = Contract;
+            WorkList = Core.DB.CompanyNikit.ToList();
+        }
+
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+
+            // если запись новая, то добавляем ее в список
+            if (CurrentService.Id == 0)
+                Core.DB.ContractsNikit.Add(CurrentService);
+
+            // сохранение в БД
+            try
+            {
+                Core.DB.SaveChanges();
+            }
+            catch
+            {
+            }
+            DialogResult = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
